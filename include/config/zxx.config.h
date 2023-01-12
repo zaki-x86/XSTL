6958495c4e2701c27cfcb4f5a11d8bb0cd3d452a
Mohamed Zaki <mamadovzicx@gmail.com>
2023-01-12 21:03:15 +0200

    Fixed definitions for unix, msvc is solved

--- include/config/zxx.config.h
+++ include/config/zxx.config.h
@@ -5,11 +5,11 @@
 #ifndef ZXX_PUBLIC
 #ifndef ZXX_INTERNAL
 
-    #ifdef WIN32 || _WIN32
+    #ifdef _WIN32
         #if defined(BUILD_SHARED)
             #define ZXX_PUBLIC __declspec(dllexport)
             #define ZXX_INTERNAL
-        #elif defined(BUILD_STATIC)
+        #elif !defined(BUILD_SHARED)
             #define ZXX_PUBLIC __declspec(dllimport)
             #define ZXX_INTERNAL
         #else
@@ -18,15 +18,8 @@
         #endif 
 
     #else
-         #if defined(BUILD_SHARED)
-            #define ZXX_PUBLIC __attribute__((visibility("default")))
-            #define ZXX_INTERNAL __attribute__((visibility("hidden")))
-        #elif defined(BUILD_STATIC)
-            #define ZXX_PUBLIC __attribute__((visibility("default")))
-            #define ZXX_INTERNAL __attribute__((visibility("hidden")))
-        #else
-            #define ZXX_PUBLIC
-        #endif 
+        #define ZXX_PUBLIC __attribute__((visibility("default")))
+        #define ZXX_INTERNAL __attribute__((visibility("hidden")))
 
 #endif // WIN32 || _WIN32
 
