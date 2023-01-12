c3b08388aa87c267f3b21727c2e672df0346e566
Mohamed Zaki <mamadovzicx@gmail.com>
2023-01-12 19:54:26 +0200

    Updated config file
    Added marker for PUBLIC and INTERNAL library components

--- include/config/zxx.config.h
+++ include/config/zxx.config.h
@@ -2,71 +2,101 @@
 
 #define _CONFIG_H_
 
+#ifndef ZXX_PUBLIC
+#ifndef ZXX_INTERNAL
+
+    #ifdef WIN32 || _WIN32
+        #if defined(XMATH_BUILD_SHARED)
+            #define ZXX_PUBLIC __declspec(dllexport)
+            #define ZXX_INTERNAL
+        #elif defined(XMATH_BUILD_STATIC)
+            #define ZXX_PUBLIC __declspec(dllimport)
+            #define ZXX_INTERNAL
+        #else
+            #define ZXX_PUBLIC
+            #define ZXX_INTERNAL
+        #endif 
+
+    #else
+         #if defined(BUILD_SHARED_LIBS)
+            #define ZXX_PUBLIC __attribute__((visibility("default")))
+            #define ZXX_INTERNAL __attribute__((visibility("hidden")))
+        #elif !defined(BUILD_SHARED_LIBS)
+            #define ZXX_PUBLIC __attribute__((visibility("default")))
+            #define ZXX_INTERNAL __attribute__((visibility("hidden")))
+        #else
+            #define ZXX_PUBLIC
+        #endif 
+
+#endif // WIN32 || _WIN32
+
+#endif // !ZXX_INTERNAL
+#endif // !ZXX_PUBLIC
 
 #if __cplusplus > 201703L
-// C++20 and later
-#define DARRAY_HAS_CONSTEXPR 1
-#define DARRAY_HAS_NODISCARD 1
-#define DARRAY_HAS_INLINE_VARIABLES 1
+    // C++20 and later
+    #define DARRAY_HAS_CONSTEXPR 1
+    #define DARRAY_HAS_NODISCARD 1
+    #define DARRAY_HAS_INLINE_VARIABLES 1
 #elif __cplusplus > 201402L
-// C++17
-#define DARRAY_HAS_CONSTEXPR 1
-#define DARRAY_HAS_NODISCARD 1
+    // C++17
+    #define DARRAY_HAS_CONSTEXPR 1
+    #define DARRAY_HAS_NODISCARD 1
 #elif __cplusplus > 201103L
 // C++11
-#define HAS_CONSTEXPR 1
+    #define HAS_CONSTEXPR 1
 #else
 // C++98/03
 #endif
 
 #ifdef HAS_CONSTEXPR
-#define ZXX_CONSTEXPR constexpr
+    #define ZXX_CONSTEXPR constexpr
 #else
-#define ZXX_CONSTEXPR
+    #define ZXX_CONSTEXPR
 #endif
 
 #ifdef HAS_NODISCARD
-#define ZXX_NODISCARD [[nodiscard]]
+    #define ZXX_NODISCARD [[nodiscard]]
 #else
-#define ZXX_NODISCARD
+    #define ZXX_NODISCARD
 #endif
 
 #ifdef CAN_FORCE_INLINE
-#define ZXX_FORCE_INLINE inline
+    #define ZXX_FORCE_INLINE inline
 #else
-#define ZXX_FORCE_INLINE
+    #define ZXX_FORCE_INLINE
 #endif
 
 
 #if __cplusplus >= 201703L
 
-#define BEGIN_NS_ZXX_CORE_CONTAINER namespace zxx::core::container {
-#define END_NS_ZXX_CORE_CONTAINER }
+    #define BEGIN_NS_ZXX_CORE_CONTAINER namespace zxx::core::container {
+    #define END_NS_ZXX_CORE_CONTAINER }
 
 #else
 
-#define BEGIN_NS_ZXX_CORE_CONTAINER             \
-    namespace zxx {                             \
-            namespace core {                    \
-                namespace container {           
+    #define BEGIN_NS_ZXX_CORE_CONTAINER             \
+        namespace zxx {                             \
+                namespace core {                    \
+                    namespace container {           
 
-#define END_NS_ZXX_CORE_CONTAINER  }}}
+    #define END_NS_ZXX_CORE_CONTAINER  }}}
 #endif
 
 #if __cplusplus >= 201703L
 
-#define BEGIN_NS_ZXX_CORE_CONTAINER_TEST namespace zxx::core::container::test {
-#define END_NS_ZXX_CORE_CONTAINER_TEST }
+    #define BEGIN_NS_ZXX_CORE_CONTAINER_TEST namespace zxx::core::container::test {
+    #define END_NS_ZXX_CORE_CONTAINER_TEST }
 
 #else
 
-#define BEGIN_NS_ZXX_CORE_CONTAINER_TEST        \
-    namespace zxx {                             \
-            namespace core {                    \
-                namespace container {           \
-                    namespace test {            
+    #define BEGIN_NS_ZXX_CORE_CONTAINER_TEST        \
+        namespace zxx {                             \
+                namespace core {                    \
+                    namespace container {           \
+                        namespace test {            
 
-#define END_NS_ZXX_CORE_CONTAINER_TEST  }}}}
+    #define END_NS_ZXX_CORE_CONTAINER_TEST  }}}}
 #endif
 
 
