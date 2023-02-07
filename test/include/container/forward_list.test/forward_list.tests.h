7679edfb692925306035df055c27b75d5fb5a575
Mohamed Zaki <mamadovzicx@gmail.com>
2023-02-07 13:22:13 +0200

    Changed include and test directory structure and fixed errors

---
+++ test/include/container/forward_list.test/forward_list.tests.h
@@ -0,0 +1,40 @@
+#ifndef _FORWARD_LIST_TESTS_H_
+
+#define _FORWARD_LIST_TESTS_H_
+
+#include "container/forward_list.test/forward_list.fixture.h"
+#include <iostream>
+#include <forward_list>
+
+BEGIN_NS_ZXX_CORE_CONTAINER_TEST
+
+TYPED_TEST_SUITE_P(forward_list_test);
+
+
+TYPED_TEST_P(forward_list_test, DefaultConstructorTest) 
+{
+
+}
+
+REGISTER_TYPED_TEST_SUITE_P(forward_list_test,
+    DefaultConstructorTest
+
+);
+
+using ForwardListTestTypeParameters = ::testing::Types<
+    char, 
+    int, 
+    long, 
+    char*,
+    int*,
+    std::string*, 
+    std::string, 
+    std::forward_list<int>
+    >;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(forward_list_test, forward_list_test, ForwardListTestTypeParameters);
+
+END_NS_ZXX_CORE_CONTAINER_TEST
+
+
+#endif // !_ARRAY_TESTS_H_
---
+++ test/include/container/multimap.test/multimap.tests.h
@@ -0,0 +1,42 @@
+
+#ifndef _MULTIMAP_TESTS_H_
+
+#define _MULTIMAP_TESTS_H_
+
+#include "container/multimap.test/multimap.fixture.h"
+#include <iostream>
+#include <map>
+
+BEGIN_NS_ZXX_CORE_CONTAINER_TEST
+
+TYPED_TEST_SUITE_P(multimap_test);
+
+
+TYPED_TEST_P(multimap_test, DefaultConstructorTest) 
+{
+
+}
+
+REGISTER_TYPED_TEST_SUITE_P(multimap_test,
+    DefaultConstructorTest
+
+);
+
+using MultimapTestTypeParameters = ::testing::Types<
+    char, 
+    int, 
+    long, 
+    char*,
+    int*,
+    std::string*, 
+    std::string, 
+    std::multimap<std::string, int>
+    >;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(multimap_Test, multimap_test, MultimapTestTypeParameters);
+
+END_NS_ZXX_CORE_CONTAINER_TEST
+
+
+#endif // !_MULTIMAP_TESTS_H_ 
+    
