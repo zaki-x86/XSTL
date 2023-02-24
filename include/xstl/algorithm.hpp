243b5746e3b0fcb9ca1bada4d6bd58f7bb8140cd
MamadovZaki <mamadovzicx@gmail.com>
2023-02-24 14:32:04 +0200

    Initialized project include and source files

---
+++ include/xstl/algorithm.hpp
@@ -0,0 +1,17 @@
+#include "config/config.h"
+
+
+_BEGIN_XSTL
+
+int algorithm_test1();
+int algorithm_test2();
+
+_END_XSTL
+ 
+int xstl::algorithm_test1() {
+    return 1;
+}
+
+int xstl::algorithm_test2() {
+    return 2;
+} 
