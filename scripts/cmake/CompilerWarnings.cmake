f4ca20292c6fbe25f7e574f81adc08c6c600a3a2
MamadovZaki <mamadovzicx@gmail.com>
2023-03-03 20:52:23 +0200

    Corrected compiler warnings

--- scripts/cmake/CompilerWarnings.cmake
+++ scripts/cmake/CompilerWarnings.cmake
@@ -59,7 +59,7 @@ function(set_ci_project_warnings project_name)
          -no-coverage-mismatch
       )
       
-   elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") AND (CMAKE_CXX_COMPILER_VERSION VERSUIN_LESS_EQUAL 10))
+   elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") AND (CMAKE_CXX_COMPILER_VERSION VERSION_LESS_EQUAL 10))
       message(STATUS "Detected clang version less than 10")
       set(CLANG_CXXFLAGS
          -Wpedantic
@@ -419,39 +419,39 @@ function(set_ci_project_warnings project_name)
 endfunction()
 
 function(set_project_warnings project_name)
-  set(MSVC_WARNINGS
+   set(MSVC_WARNINGS
       /W4     # Baseline reasonable warnings
       /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss
-              # of data
+            # of data
       /w14254 # 'operator': conversion from 'type1:field_bits' to
-              # 'type2:field_bits', possible loss of data
+            # 'type2:field_bits', possible loss of data
       /w14263 # 'function': member function does not override any base class
-              # virtual member function
+            # virtual member function
       /w14265 # 'classname': class has virtual functions, but destructor is not
-              # virtual instances of this class may not be destructed correctly
+            # virtual instances of this class may not be destructed correctly
       /w14287 # 'operator': unsigned/negative constant mismatch
       /we4289 # nonstandard extension used: 'variable': loop control variable
-              # declared in the for-loop is used outside the for-loop scope
+            # declared in the for-loop is used outside the for-loop scope
       /w14296 # 'operator': expression is always 'boolean_value'
       /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
       /w14545 # expression before comma evaluates to a function which is missing
-              # an argument list
+            # an argument list
       /w14546 # function call before comma missing argument list
       /w14547 # 'operator': operator before comma has no effect; expected
-              # operator with side-effect
+            # operator with side-effect
       /w14549 # 'operator': operator before comma has no effect; did you intend
-              # 'operator'?
+            # 'operator'?
       /w14555 # expression has no effect; expected expression with side- effect
       /w14619 # pragma warning: there is no warning number 'number'
       /w14640 # Enable warning on thread un-safe static member initialization
       /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may
-              # cause unexpected runtime behavior.
+            # cause unexpected runtime behavior.
       /w14905 # wide string literal cast to 'LPSTR'
       /w14906 # string literal cast to 'LPWSTR'
       /w14928 # illegal copy-initialization; more than one user-defined
-              # conversion has been implicitly applied
+            # conversion has been implicitly applied
       /permissive- # standards conformance mode for MSVC compiler.
-  )
+   )
 
   set(CLANG_WARNINGS
       -Wall
